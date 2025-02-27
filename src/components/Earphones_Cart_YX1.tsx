351ae133ee9cc2c5a109b498b4cecf51ad6892c1
Void-404X <alisherwashere@gmail.com>
2025-02-27 19:47:33 +0800

    Complete Main Page

--- src/components/Earphones_Cart_YX1.tsx
+++ src/components/Earphones_Cart_YX1.tsx
@@ -4,10 +4,10 @@ import yx1earphonemd from "../assets/icons/yxearphonemd.svg";
 
 const Earphones_Cart_YX1: React.FC = () => {
   return (
-    <div className="md:flex md:justify-between mx-6 md:mx-10 lg:mx-10 xl:mx-20 2xl:mx-40 lg:flex lg:justify-end space-x-4">
-      <div className="md:h-80">
+    <div className="flex flex-col md:flex-row items-center justify-center mx-6 md:mx-10 lg:mx-10 xl:mx-20 2xl:mx-40 space-x-0 md:space-x-4 lg:space-x-12  text-center">
+      <div className="md:h-80 flex items-center justify-center">
         <img
-          className="w-full h-auto object-cover md:hidden"
+          className="w-100 h-auto object-cover md:hidden"
           src={yx1earphone}
           alt="yx1earphone"
         />
@@ -17,7 +17,7 @@ const Earphones_Cart_YX1: React.FC = () => {
           alt="yx1earphonemd"
         />
       </div>
-      <div className="max-w-screen-lg mt-8 md:mt-0 h-60 md:w-84 md:h-80 bg-[#F1F1F1] p-6 rounded-lg flex flex-col items-center justify-center">
+      <div className="w-100 max-w-screen-lg mt-8 md:mt-0 h-60 md:w-84 md:h-80 bg-[#F1F1F1] p-6 rounded-lg flex flex-col items-center justify-center">
         <h2 className="text-3xl mt-4 font-bold text-black tracking-wide">
           YX1 EARPHONES
         </h2>
